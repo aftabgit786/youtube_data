21e44737b9699ca6faa1cf6a54b1682f53104d9c
Muhammad Aftab <66480543+aftabgit786@users.noreply.github.com>
2023-04-18 14:50:14 +0500

    Revert "T2"

--- utils.py
+++ utils.py
@@ -1,7 +1,4 @@
 import time
-import os
-import pandas as pd
-from bs4 import BeautifulSoup
 
 
 def scroll_to_page_end(driver):
@@ -33,60 +30,3 @@ def extract_comments_data(soup):
         comments.append([user_name, thumbnail_url, comment_time, likes, comment_text])
 
     return comments
-
-
-def get_channel_directory(channel_url):
-    channel_name = channel_url.split("/")[-2].replace('@', '')
-    channel_dir = os.path.join(os.getcwd(), channel_name)
-    if not os.path.exists(channel_dir):
-        os.mkdir(channel_dir)
-    return channel_dir
-
-
-def get_video_links(soup):
-    video_links = []
-    for video in soup.find_all('ytd-rich-grid-media'):
-        video_url = 'https://www.youtube.com' + video.find('a', {'id': 'video-title-link'}).get('href')
-        video_links.append(video_url)
-    return video_links
-
-
-def process_video_comments(video_link, channel_dir, driver):
-    driver.get(video_link)
-
-    time.sleep(5)
-
-    scroll_to_page_end(driver)
-
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-
-    comments_data = extract_comments_data(soup)
-
-    video_name = soup.title.string.split(" - YouTube")[0]
-    video_title = video_name.replace('/', '-')
-
-    file_name = video_title + '.csv'
-    file_path = os.path.join(channel_dir, file_name)
-    df = pd.DataFrame(comments_data, columns=['User Name', 'Thumbnail URL', 'Comment Time', 'Likes', 'Comment Text'])
-    df.to_csv(file_path, index=False)
-    print(f"Comments for video {video_title} saved to CSV file {file_name}")
-
-def process_channel_videos(channel_url, driver):
-    channel_url += "/videos"
-    driver.get(channel_url)
-
-    time.sleep(5)
-
-    scroll_to_page_end(driver)
-
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-
-    channel_dir = get_channel_directory(channel_url)
-    video_links = get_video_links(soup)
-
-    for video_link in video_links:
-        process_video_comments(video_link, channel_dir, driver)
-
-    print("Data successfully saved to CSV files.")
